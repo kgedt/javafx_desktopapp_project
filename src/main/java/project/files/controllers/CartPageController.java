9719c9b60abb0aa320bb6cb068fa8a9a89e2db9e
kgedt <magzumov7162@gmail.com>
2023-05-09 18:46:23 +0600

    cartPage added

---
+++ src/main/java/project/files/controllers/CartPageController.java
@@ -0,0 +1,30 @@
+package project.files.controllers;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class CartPageController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    void backClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+
+
+    }
+}
